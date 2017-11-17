1e588bbdaf5a18b00c62eb88522edeb07b555ef9
kcq218 <kennyqsoftware@gmail.com>
2017-11-16 18:26:50 -0800

    fix attempt

--- app/src/main/java/com/jenniferlam/career_light_project/CDescriptionActivity.java
+++ app/src/main/java/com/jenniferlam/career_light_project/CDescriptionActivity.java
@@ -15,15 +15,6 @@ public class CDescriptionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_course_description);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
